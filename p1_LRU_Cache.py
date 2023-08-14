52ef72c851c9b71893838ddd1df830e27e04a999
Chad Hanson <chad.hanson@stellantis.com>
2023-08-14 09:20:09 -0400

    first run at LRU project

--- p1_LRU_Cache.py
+++ p1_LRU_Cache.py
@@ -1,23 +1,32 @@
+from collections import OrderedDict
+
+
 class LRU_Cache(object):
 
-    def __init__(self, capacity):
-        # Initialize class variables
-        pass
+    def __init__(self, capacity = 5):
+        self.capacity = capacity
+        self.our_cache = OrderedDict()
+        
 
     def get(self, key):
-        # Retrieve item from provided key. Return -1 if nonexistent. 
-        pass
+        if key not in self.our_cache:
+            return -1
+        else:
+            self.our_cache.move_to_end(key)
+            return self.our_cache[key]
 
     def set(self, key, value):
-        # Set the value if the key is not present in the cache. If the cache is at capacity remove the oldest item. 
-        pass
+        self.our_cache[key] = value
+        self.our_cache.move_to_end(key)
+        if len(self.our_cache) > self.capacity:
+            self.our_cache.popitem(last = False)
 
 our_cache = LRU_Cache(5)
 
-our_cache.set(1, 1);
-our_cache.set(2, 2);
-our_cache.set(3, 3);
-our_cache.set(4, 4);
+our_cache.set(1, 1)
+our_cache.set(2, 2)
+our_cache.set(3, 3)
+our_cache.set(4, 4)
 
 
 our_cache.get(1)       # returns 1
@@ -28,12 +37,20 @@ our_cache.set(5, 5)
 our_cache.set(6, 6)
 
 our_cache.get(3)      # returns -1 because the cache reached it's capacity and 3 was the least recently used entry
-
 ## Add your own test cases: include at least three test cases
 ## and two of them must include edge cases, such as null, empty or very large values
 
 ## Test Case 1
-
+our_cache.set(7, 7)
+print(our_cache.get(7))
+print(our_cache.our_cache)
 ## Test Case 2
-
+our_cache.set(2, "test")
+print(our_cache.get(2))
+print(our_cache.our_cache)
 ## Test Case 3
+our_cache.set(1, 8675309)
+print(our_cache.get(1))
+print(our_cache.our_cache)
+## Test Case 4
+print(our_cache.get(3))
