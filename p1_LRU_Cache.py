033da7df9215672e882efbf0b5221f9a356b9b07
Chad Hanson <chad.hanson@stellantis.com>
2023-08-14 10:00:19 -0400

    added additional print statements

--- p1_LRU_Cache.py
+++ p1_LRU_Cache.py
@@ -27,14 +27,17 @@ our_cache.set(1, 1)
 our_cache.set(2, 2)
 our_cache.set(3, 3)
 our_cache.set(4, 4)
+print(our_cache.our_cache)
 
 our_cache.get(1)  # returns 1
 our_cache.get(2)  # returns 2
-our_cache.get(9)  # returns -1 because 9 is not present in the cache
+print(our_cache.get(9))  # returns -1 because 9 is not present in the cache
+print(our_cache.our_cache)
 
 our_cache.set(5, 5)
 our_cache.set(6, 6)
-our_cache.get(3)  # returns -1 because 3 was the LRU entry
+print(our_cache.get(3))  # returns -1 because 3 was the LRU entry
+print(our_cache.our_cache)
 
 # Test Case 1
 our_cache.set(7, 7)
@@ -50,3 +53,5 @@ print(our_cache.get(9))
 print(our_cache.our_cache)
 # Test Case 4
 print(our_cache.get(3))
+# Test Case 5
+print(our_cache.get(5))
