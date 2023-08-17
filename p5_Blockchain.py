d68875338e009ffdd0e9a6ceaad3d291e6dd1c82
Chad Hanson <chad.hanson@stellantis.com>
2023-08-17 10:47:02 -0400

    first draft

--- p5_Blockchain.py
+++ p5_Blockchain.py
@@ -1,28 +1,54 @@
 import hashlib
-
-def calc_hash(self):
-      sha = hashlib.sha256()
-
-      hash_str = "We are going to encode this string of data!".encode('utf-8')
-
-      sha.update(hash_str)
-
-      return sha.hexdigest()
+from time import gmtime, strftime
+import time
 
 
 class Block:
+    def __init__(self, data, previous_hash, next=None):
+        self.timestamp = time.gmtime()
+        self.data = data
+        self.previous_hash = previous_hash
+        self.hash = self.calc_hash()
+        self.next = next
 
-    def __init__(self, timestamp, data, previous_hash):
-      self.timestamp = timestamp
-      self.data = data
-      self.previous_hash = previous_hash
-      self.hash = self.calc_hash()
+    def calc_hash(self):
+        sha = hashlib.sha256()
+        hash_str = f"{self.timestamp}-{self.data}-{self.previous_hash}".encode('utf-8')
+        sha.update(hash_str)
+        return sha.hexdigest()
 
-## Add your own test cases: include at least three test cases
-## and two of them must include edge cases, such as null, empty or very large values
 
-## Test Case 1
+class BlockChain:
+    def __init__(self, head=None, next=None):
+        self.head = head
+        self.next = next
 
-## Test Case 2
+    def add_block(self, data):
+        if self.head == None:
+            self.head = Block(data, None)
+        else:
+            current = self.head
+            while current.next:
+                current = current.next
+            previous_hash = current.hash
+            current.next = Block(data, previous_hash)
 
-## Test Case 3
+    def print_block(self):
+        if self.head is None:
+            print("The blockchain is empty.")
+            quit()
+        else:
+            current = self.head
+        while current:
+            print("Data: ", current.data)
+            print("Hash: ", current.hash)
+            print("Previous hash: ", current.previous_hash)
+            print("Time: ", time.strftime("%a, %d %b %Y %I:%M:%S %p GMT", time.gmtime()))  # noqa
+            current = current.next
+
+
+my_blockchain = BlockChain()
+my_blockchain.add_block("block1")
+my_blockchain.add_block("block2")
+my_blockchain.add_block("block3")
+my_blockchain.print_block()
