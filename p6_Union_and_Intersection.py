8b83d04826f7eea4591666b50a4eff395689498a
Chad Hanson <chad.hanson@stellantis.com>
2023-08-18 09:49:06 -0400

    first draft

--- p6_Union_and_Intersection.py
+++ p6_Union_and_Intersection.py
@@ -1,94 +1,80 @@
 class Node:
-    def __init__(self, value):
+    def __init__(self, value=None, next=None):
         self.value = value
-        self.next = None
-
-    def __repr__(self):
-        return str(self.value)
+        self.next = next
 
 
 class LinkedList:
-    def __init__(self):
+    def __init__(self, length=0):
         self.head = None
+        self.length = length
 
-    def __str__(self):
-        cur_head = self.head
-        out_string = ""
-        while cur_head:
-            out_string += str(cur_head.value) + " -> "
-            cur_head = cur_head.next
-        return out_string
+    def insert_node(self, value, length=1):
+        node = Node(value, self.head)
+        self.head = node
+        self.length = length
 
 
-    def append(self, value):
-
-        if self.head is None:
-            self.head = Node(value)
-            return
-
-        node = self.head
-        while node.next:
-            node = node.next
-
-        node.next = Node(value)
-
-    def size(self):
-        size = 0
-        node = self.head
-        while node:
-            size += 1
-            node = node.next
-
-        return size
-
-def union(llist_1, llist_2):
-    # Your Solution Here
-    pass
-
-def intersection(llist_1, llist_2):
-    # Your Solution Here
-    pass
+def display_list(list):
+    temp = list
+    while temp:
+        print(temp.value, end="\t")
+        temp = temp.next
+    print("")
 
 
-## Test case 1
+def check_list(list, value):
+    temp = list
+    while temp:
+        if temp.value is value:
+            return True
+        temp = temp.next
+    return False
 
-linked_list_1 = LinkedList()
-linked_list_2 = LinkedList()
 
-element_1 = [3,2,4,35,6,65,6,4,3,21]
-element_2 = [6,32,4,9,6,1,11,21,1]
+def union(list1, list2):
+    union = LinkedList()
+    temp = list1
+    while temp:
+        union.insert_node(temp.value)
+        temp = temp.next
 
-for i in element_1:
-    linked_list_1.append(i)
+    temp = list2
+    while temp:
+        if not check_list(union.head, temp.value):
+            union.insert_node(temp.value)
+        temp = temp.next
+    return union.head
 
-for i in element_2:
-    linked_list_2.append(i)
 
-print (union(linked_list_1,linked_list_2))
-print (intersection(linked_list_1,linked_list_2))
+def intersection(list1, list2):
+    intersection = LinkedList()
+    temp = list1
+    while temp:
+        if check_list(list2, temp.value):
+            intersection.insert_node(temp.value)
 
-## Test case 2
+    return intersection.head
 
-linked_list_3 = LinkedList()
-linked_list_4 = LinkedList()
+list1 = LinkedList()
+list1.insert_node(5)
+list1.insert_node(6)
+list1.insert_node(7)
+list1.insert_node(8)
+list1.insert_node(9)
+list1.insert_node(10)
+# print("Linked list 1: ")
+# display_list(list1.head)
 
-element_1 = [3,2,4,35,6,65,6,4,3,23]
-element_2 = [1,7,8,9,11,21,1]
+list2 = LinkedList()
+list2.insert_node(0)
+list2.insert_node(1)
+list2.insert_node(2)
+list2.insert_node(3)
+list2.insert_node(4)
+# print("Linked list 2: ")
+# display_list(list2.head)
 
-for i in element_1:
-    linked_list_3.append(i)
-
-for i in element_2:
-    linked_list_4.append(i)
-
-print (union(linked_list_3,linked_list_4))
-print (intersection(linked_list_3,linked_list_4))
-
-## Add your own test cases: include at least three test cases
-## and two of them must include edge cases, such as null, empty or very large values
-
-## Test Case 1
-
-## Test Case 2
-
-## Test Case 3
+print("The union of lists 1 and 2 is: ")
+union = union(list1.head, list2.head)
+display_list(union)
