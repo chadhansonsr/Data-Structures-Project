1a21e7596fd961b3b47d973e3678b0486f3762d5
Chad Hanson <chad.hanson@stellantis.com>
2023-08-21 07:48:09 -0400

    final draft

--- p1_LRU_Cache.py
+++ p1_LRU_Cache.py
@@ -27,31 +27,31 @@ our_cache.set(1, 1)
 our_cache.set(2, 2)
 our_cache.set(3, 3)
 our_cache.set(4, 4)
-print(our_cache.our_cache)
+print(our_cache.our_cache)  # prints key/value pairs currently in dictionary
 
-our_cache.get(1)  # returns 1 and moves key to end
-our_cache.get(2)  # returns 2 and moves key to end dictionary 
-print(our_cache.get(9))  # returns -1 because 9 is not present in the cache
+our_cache.get(1)
+our_cache.get(2)
+print(our_cache.get(9))  # prints -1 because value not in cache
 print(our_cache.our_cache)
 
 our_cache.set(5, 5)
 our_cache.set(6, 6)
-print(our_cache.get(3))  # returns -1 because 3 was the LRU entry
+print(our_cache.get(3))  # prints -1 because value not in cache
 print(our_cache.our_cache)
 
 # Test Case 1
 our_cache.set(7, 7)
-print(our_cache.get(7))
+print(our_cache.get(7))  # prints 7
 print(our_cache.our_cache)
 # Test Case 2
 our_cache.set(8, "test")
-print(our_cache.get(8))
+print(our_cache.get(8))  # prints "test"
 print(our_cache.our_cache)
 # Test Case 3
 our_cache.set(9, 8675309)
-print(our_cache.get(9))
+print(our_cache.get(9))  # prints 8675309
 print(our_cache.our_cache)
 # Test Case 4
-print(our_cache.get(3))
+print(our_cache.get(3))  # prints -1 because value not in cache
 # Test Case 5
-print(our_cache.get(5))
+print(our_cache.get(5))  # prints 5
