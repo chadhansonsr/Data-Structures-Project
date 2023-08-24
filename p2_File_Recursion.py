3c3ddd391e2fb298d9aefb1c946bf9fe2b081545
Chad Hanson <chad.hanson@stellantis.com>
2023-08-24 14:48:00 -0400

    removed glob method and utilized os module

--- p2_File_Recursion.py
+++ p2_File_Recursion.py
@@ -1,28 +1,43 @@
 import os
 
+
 def find_files(suffix, path):
-    """
-    Find all files beneath path with file name suffix.
+    matches = []
+    for entry in os.listdir(path):
+        path_dir = os.path.join(path, entry)
+        if os.path.isfile(path_dir) and path_dir.endswith(suffix):
+            matches.append(path_dir)
+        elif os.path.isdir(path_dir):
+            matches.extend(find_files(suffix, path_dir))
+    return matches
 
-    Note that a path may contain further subdirectories
-    and those subdirectories may also contain further subdirectories.
 
-    There are no limit to the depth of the subdirectories can be.
+search1 = find_files(".c", r"\Users\t9349ch\Desktop\SWX\Udacity\Data Structures Project")  # noqa
+print("\nHere are the files ending with .c.")
+for search in search1:
+    print(search)
+    # search will return
+    # \Users\t9349ch\Desktop\SWX\Udacity\Data Structures Project\testdir\t1.c  # noqa
+    # \Users\t9349ch\Desktop\SWX\Udacity\Data Structures Project\testdir\subdir1\a.c  # noqa
+    # \Users\t9349ch\Desktop\SWX\Udacity\Data Structures Project\testdir\subdir3\subsubdir1\b.c  # noqa
+    # \Users\t9349ch\Desktop\SWX\Udacity\Data Structures Project\testdir\subdir5\a.c  # noqa
 
-    Args:
-      suffix(str): suffix if the file name to be found
-      path(str): path of the file system
+search2 = find_files(".jpg", r"\Users\t9349ch\Desktop\SWX\Udacity\Data Structures Project")  # noqa
+print("\n\nHere are the files ending with .jpg.")
+for search in search2:
+    print(search)
+    # search will be blank because there are no .jpg files
 
-    Returns:
-       a list of paths
-    """
-    return None
-
-## Add your own test cases: include at least three test cases
-## and two of them must include edge cases, such as null, empty or very large values
-
-## Test Case 1
-
-## Test Case 2
-
-## Test Case 3
+search3 = find_files(".c", r"\Users\t9349ch\Desktop\SWX\Udacity")  # noqa
+print("\n\nHere are the files containing with .c.")
+for search in search3:
+    print(search)
+    # this will search a level above the current folder in the directory  # noqa
+    # \Users\t9349ch\Desktop\SWX\Udacity\Data Structures Project\testdir\subdir1\a.c  # noqa
+    # \Users\t9349ch\Desktop\SWX\Udacity\Data Structures Project\testdir\subdir3\subsubdir1\b.c  # noqa
+    # \Users\t9349ch\Desktop\SWX\Udacity\Data Structures Project\testdir\subdir5\a.c  # noqa
+    # \Users\t9349ch\Desktop\SWX\Udacity\Data Structures Project\testdir\t1.c
+    # \Users\t9349ch\Desktop\SWX\Udacity\Side Projects\Python\venv\Lib\site-packages\httptools\parser\parser.c  # noqa
+    # \Users\t9349ch\Desktop\SWX\Udacity\Side Projects\Python\venv\Lib\site-packages\httptools\parser\url_parser.c  # noqa
+    # \Users\t9349ch\Desktop\SWX\Udacity\Side Projects\Python\venv\Lib\site-packages\markupsafe\_speedups.c  # noqa
+    # \Users\t9349ch\Desktop\SWX\Udacity\Side Projects\Python\venv\Lib\site-packages\websockets\speedups.c  # noqa
