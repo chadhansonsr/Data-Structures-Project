6bd0e80c50540c4d7c7c8fb0c8339ed1bf90cf91
Chad Hanson <chad.hanson@stellantis.com>
2023-08-24 14:12:21 -0400

    first copy not using glob method

--- p2_File_Recursion.py
+++ p2_File_Recursion.py
@@ -1,24 +1,28 @@
-import glob
+import os
 
-search1 = sorted(glob.glob(r"\Users\t9349ch\Desktop\SWX\Udacity\Data Structures Project/**/*.c", recursive=True))  # noqa
-print("Here are the files ending with .c.")
-for search in search1:
-    print(search)
-    # search will return
-    # \Users\t9349ch\Desktop\SWX\Udacity\Data Structures Project\testdir\t1.c  # noqa
-    # \Users\t9349ch\Desktop\SWX\Udacity\Data Structures Project\testdir\subdir1\a.c  # noqa
-    # \Users\t9349ch\Desktop\SWX\Udacity\Data Structures Project\testdir\subdir3\subsubdir1\b.c  # noqa
-    # \Users\t9349ch\Desktop\SWX\Udacity\Data Structures Project\testdir\subdir5\a.c  # noqa
+def find_files(suffix, path):
+    """
+    Find all files beneath path with file name suffix.
 
+    Note that a path may contain further subdirectories
+    and those subdirectories may also contain further subdirectories.
 
-search2 = sorted(glob.glob(r"\Users\t9349ch\Desktop\SWX\Udacity\Data Structures Project/**/*.jpg", recursive=True))  # noqa
-print("Here are the files ending with .jpg.")
-for search in search2:
-    print(search)
-    # search will be blank because there are no .jpg files
+    There are no limit to the depth of the subdirectories can be.
 
-search3 = sorted(glob.glob(r"\my location has a typo/**/*.c", recursive=True))
-print("Here are the files ending with .c.")
-for search in search3:
-    print(search)
-    # search will be blank because of a directory typo
+    Args:
+      suffix(str): suffix if the file name to be found
+      path(str): path of the file system
+
+    Returns:
+       a list of paths
+    """
+    return None
+
+## Add your own test cases: include at least three test cases
+## and two of them must include edge cases, such as null, empty or very large values
+
+## Test Case 1
+
+## Test Case 2
+
+## Test Case 3
